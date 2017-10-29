21f6e6d624c2f8d808b7a35e5862fd192dc298f7
Saifi <Syed.Saifi@emc.com>
2017-10-29 19:14:27 +0530

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAp_Z46FYMr3p8y2zQ3M-ZpSN21dstC1AY",
+    authDomain: "angularshop-e4f48.firebaseapp.com",
+    databaseURL: "https://angularshop-e4f48.firebaseio.com",
+    projectId: "angularshop-e4f48",
+    storageBucket: "angularshop-e4f48.appspot.com",
+    messagingSenderId: "1023722900949"
+  }
+};
