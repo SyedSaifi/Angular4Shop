48e465aa0a3bf9799a01920bf3bc4b52ce5d4d1a
Saifi <Syed.Saifi@emc.com>
2017-10-30 23:41:14 +0530

    Google authentication and Url Redirect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './auth.service';
 import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -18,6 +19,7 @@ import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { LoginComponent } from './login/login.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AuthGuard } from "./auth-guard.service";
 
 @NgModule({
   declarations: [
@@ -43,16 +45,20 @@ import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.componen
       {path:'', component: HomeComponent},
       {path:'products', component: ProductsComponent},
       {path:'shopping-cart', component: ShoppingCartComponent},
-      {path:'check-out', component: CheckOutComponent},
-      {path:'order-success', component: OrderSuccessComponent},
       {path:'login', component: LoginComponent},
-      {path:'my/orders', component: MyOrdersComponent},
-      {path:'admin/products', component: AdminProductsComponent},
-      {path:'admin/orders', component: AdminOrdersComponent},
-      {path:'login', component: LoginComponent}
+
+      {path:'check-out', component: CheckOutComponent, canActivate: [AuthGuard]},
+      {path:'order-success', component: OrderSuccessComponent, canActivate: [AuthGuard]},
+      {path:'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard]},
+
+      {path:'admin/products', component: AdminProductsComponent, canActivate: [AuthGuard]},
+      {path:'admin/orders', component: AdminOrdersComponent, canActivate: [AuthGuard]},
     ])
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,20 @@
+import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
+import {AngularFireAuth} from 'angularfire2/auth';
+import * as firebase from 'firebase';
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public auth: AuthService) { 
   }
 
+  logout(){
+    this.auth.logout();
+  }
 }
